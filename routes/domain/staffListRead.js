973b1eb6efe613789be55ff396e98f798e248fa8
Murat <murat351@narod.ru>
2022-03-07 00:40:21 +0300

    fix login and domains, fix server

--- routes/domain/staffListRead.js
+++ routes/domain/staffListRead.js
@@ -7,7 +7,8 @@ const router = express.Router({ mergeParams: true });
 router.post("/staff_list_read", function (req, res) {
     if(!(req.body.key = Number(req.body.key)) || !(req.body.count = Number(req.body.count))) return res.status(400).send("Wrong body");
     ManagerModel.findAll({where: {id: {[Op.between]: [req.body.key, req.body.key + req.body.count - 1]}}, order: [["id", "ASC"]]}).then((models) => {
-        res.send(models.map(v => [v.id, v.hash, v.updatedAt && v.updatedAt.toLocaleDateString("default", {day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric"}), v.name, v.passkey, v.createdAt && v.createdAt.toLocaleDateString("default", {day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric"})]));
+        //res.send(models.map(v => [v.id, v.hash, v.updatedAt && v.updatedAt.toLocaleDateString("default", {day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric"}), v.name, v.passkey, v.createdAt && v.createdAt.toLocaleDateString("default", {day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric"})]));
+        res.send(models.map(v => [v.id, v.name, v.role, v.createdAt && v.createdAt.toLocaleDateString("default", {day: "numeric", month: "numeric", year: "numeric", hour: "numeric", minute: "numeric", second: "numeric"})]));
     }).catch((err) => {
         if(!Number.isInteger(err)) console.log(err);
         res.status( Number.isInteger(err) ? err : 500).send("Error on server");
